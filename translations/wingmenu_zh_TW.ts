ebabd53a62cb3698b95306f8dd9dc934795eb9f7
BrLi <brli@chakralinux.org>
2022-07-18 21:39:19 +0800

    add Taiwanese translation

---
+++ translations/wingmenu_zh_TW.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW" sourcelanguage="en">
+<context>
+    <name>WingMenuConfiguration</name>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="17"/>
+        <source>WingMenu settings</source>
+        <translation type="unfinished">設定 WingMenu</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="30"/>
+        <source>General</source>
+        <translation type="unfinished">一般</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="36"/>
+        <source>Panel Button</source>
+        <translation type="unfinished">工具列按鍵</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="45"/>
+        <source>Show icon:</source>
+        <translation type="unfinished">顯示圖示：</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="73"/>
+        <location filename="../wingmenuconfiguration.ui" line="116"/>
+        <source>Browse</source>
+        <translation type="unfinished">瀏覽</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="87"/>
+        <source>Show text:</source>
+        <translation type="unfinished">顯示文字：</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="100"/>
+        <source>Menu file</source>
+        <translation type="unfinished">選單檔案</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="106"/>
+        <source>File:</source>
+        <translation type="unfinished">檔案：</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="130"/>
+        <source>Keyboard Shortcut</source>
+        <translation type="unfinished">鍵盤捷徑</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="149"/>
+        <source>Click the button to record shortcut:</source>
+        <translation type="unfinished">按下按鍵以收錄快捷鍵組合：</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="177"/>
+        <source>Display</source>
+        <translation type="unfinished">顯示</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="183"/>
+        <source>Categories on the Left</source>
+        <translation type="unfinished">左側的分類目錄</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="190"/>
+        <source>Search on Bottom</source>
+        <translation type="unfinished">底部的搜尋列</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="197"/>
+        <source>Sidebar on the Left</source>
+        <translation type="unfinished">側邊欄靠左</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="204"/>
+        <source>Reverse Sidebar</source>
+        <translation type="unfinished">倒轉側邊欄</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="211"/>
+        <source>Applications Layout:</source>
+        <translation type="unfinished">應用程式版面：</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="219"/>
+        <source>List (Name and Description)</source>
+        <translation type="unfinished">清單（名稱與描述）</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="224"/>
+        <source>List (Name only)</source>
+        <translation type="unfinished">清單（僅顯示名稱）</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="229"/>
+        <source>Icons</source>
+        <translation type="unfinished">圖示</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="240"/>
+        <source>Other</source>
+        <translation type="unfinished">其他</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="246"/>
+        <source>Switch Category on hover</source>
+        <translation type="unfinished">滑鼠掠過時切換分類</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="253"/>
+        <source> ms</source>
+        <translation type="unfinished"> 毫秒</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="256"/>
+        <source>Delay: </source>
+        <translation type="unfinished">延遲： </translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="275"/>
+        <source>Ask before removing Favorite</source>
+        <translation type="unfinished">移除最愛前提醒</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="31"/>
+        <source>Reset</source>
+        <translation type="unfinished">重設</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="49"/>
+        <source>Choose Icon File</source>
+        <translation type="unfinished">選擇圖示檔案</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="51"/>
+        <source>Images (*.png *.xpm *.svg)</source>
+        <translation type="unfinished">圖片 (*.png *.xpm *.svg)</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="57"/>
+        <source>Choose Menu File</source>
+        <translation type="unfinished">選擇選單檔案</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="59"/>
+        <source>Menu files (*.menu)</source>
+        <translation type="unfinished">選單檔案 (*.menu)</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="87"/>
+        <source>Menu</source>
+        <translation type="unfinished">選單</translation>
+    </message>
+</context>
+<context>
+    <name>WingMenuPlugin</name>
+    <message>
+        <location filename="../wingmenuplugin.cpp" line="73"/>
+        <source>Menu</source>
+        <translation type="unfinished">選單</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuplugin.cpp" line="116"/>
+        <source>Show/hide main menu</source>
+        <translation type="unfinished">顯示/隱藏主要選單</translation>
+    </message>
+</context>
+<context>
+    <name>WingMenuWidget</name>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="427"/>
+        <source>Remove from Favorites</source>
+        <translation type="unfinished">自最愛移除</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="431"/>
+        <source>Confirm removal</source>
+        <translation type="unfinished">確認移除</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="431"/>
+        <source>Are you sure you want to remove
+%1:%2
+from Favorites?
+</source>
+        <translation type="unfinished">你確定你要將
+%1:%2
+自最愛清單移除嗎？
+</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="439"/>
+        <source>Add to Favorites</source>
+        <translation type="unfinished">新增至最愛</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="463"/>
+        <source>Add to desktop</source>
+        <translation type="unfinished">新增至桌面</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="469"/>
+        <source>Question</source>
+        <translation type="unfinished">提問</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="469"/>
+        <source>A file with the same name already exists.
+Do you want to overwrite it?</source>
+        <translation type="unfinished">已存在檔案名稱相同的檔案。
+您確定要覆蓋過去嗎？</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="473"/>
+        <source>Warning</source>
+        <translation type="unfinished">警告</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="473"/>
+        <source>The file cannot be overwritten.</source>
+        <translation type="unfinished">該檔案無法覆寫。</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="478"/>
+        <source>Copy</source>
+        <translation type="unfinished">複製</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="508"/>
+        <source>Favorites</source>
+        <translation type="unfinished">最愛</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="518"/>
+        <source>All Applications</source>
+        <translation type="unfinished">所有檔案</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="717"/>
+        <source>Name</source>
+        <translation type="unfinished">名稱</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="719"/>
+        <source>Comment</source>
+        <translation type="unfinished">註釋</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="721"/>
+        <source>File</source>
+        <translation type="unfinished">檔案</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="722"/>
+        <source>Category</source>
+        <translation type="unfinished">分類</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="723"/>
+        <source>Exec</source>
+        <translation type="unfinished">執行</translation>
+    </message>
+</context>
+</TS>
