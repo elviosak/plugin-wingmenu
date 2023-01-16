3204579779950cc7ea738eace911912639a765c4
Heimen Stoffels <vistausss@fastmail.com>
2023-01-16 20:22:19 +0100

    Added Dutch translation

---
+++ translations/wingmenu_nl.ts
@@ -0,0 +1,428 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>WingMenuConfiguration</name>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="17"/>
+        <source>WingMenu settings</source>
+        <translation>WingMenu-instellingen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="30"/>
+        <source>General</source>
+        <translation>Algemeen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="36"/>
+        <source>Panel Button</source>
+        <translation>Menuknop</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="45"/>
+        <source>Show icon:</source>
+        <translation>Pictogram:</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="73"/>
+        <location filename="../wingmenuconfiguration.ui" line="116"/>
+        <source>Browse</source>
+        <translation>Kiezen…</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="87"/>
+        <source>Show text:</source>
+        <translation>Tekst:</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="100"/>
+        <source>Menu file</source>
+        <translation>Menubestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="106"/>
+        <source>File:</source>
+        <translation>Bestand:</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="130"/>
+        <source>Keyboard Shortcut</source>
+        <translation>Sneltoets</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="149"/>
+        <source>Click the button to record shortcut:</source>
+        <translation>Klik op de knop om een sneltoets toe te kennen:</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="177"/>
+        <source>Display</source>
+        <translation>Weergave</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="183"/>
+        <source>Categories on the Left</source>
+        <translation>Categorieën aan linkerzijde</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="190"/>
+        <source>Search on Bottom</source>
+        <translation>Zoekbalk aan onderzijde</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="197"/>
+        <source>Sidebar on the Left</source>
+        <translation>Zijbalk aan linkerzijde</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="204"/>
+        <source>Reverse Sidebar</source>
+        <translation>Zijbalkindeling omdraaien</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="211"/>
+        <source>Applications Layout:</source>
+        <translation>Programma-indeling:</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="219"/>
+        <source>List (Name and Description)</source>
+        <translation>Lijst (naam en beschrijving)</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="224"/>
+        <source>List (Name only)</source>
+        <translation>Lijst (naam)</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="229"/>
+        <source>Icons</source>
+        <translation>Pictogrammen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="240"/>
+        <source>Other</source>
+        <translation>Overig</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="246"/>
+        <source>Switch Category on hover</source>
+        <translation>Categorieën openen door middel van aanwijzen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="253"/>
+        <source> ms</source>
+        <translation> ms</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="256"/>
+        <source>Delay: </source>
+        <translation>Vertraging: </translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="275"/>
+        <source>Ask before removing Favorite</source>
+        <translation>Om verwijdering vragen bij verwijderen van favorieten</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="299"/>
+        <source>Leave Actions</source>
+        <translation>Energie-acties</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="305"/>
+        <source>Customize Leave Actions</source>
+        <translation>Energie-acties aanpassen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="320"/>
+        <source>Load from menu file</source>
+        <translation>Laden uit menubestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="327"/>
+        <source>Add .desktop file</source>
+        <translation>.desktop-bestand toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="350"/>
+        <source>The order displayed here ignores the option &quot;Reverse sidebar&quot;.</source>
+        <translation>Onderstaande volgorde negeert op de optie ‘Zijbalkindeling omdraaien’.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="394"/>
+        <source>New</source>
+        <translation>Nieuw</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="405"/>
+        <source>Edit</source>
+        <translation>Bewerken</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="432"/>
+        <source>Up</source>
+        <translation>Omhoog</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="443"/>
+        <source>Down</source>
+        <translation>Omlaag</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.ui" line="470"/>
+        <source>Remove</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="43"/>
+        <source>Reset</source>
+        <translation>Standaardwaarden</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="61"/>
+        <source>Choose Icon File</source>
+        <translation>Kies een pictogrambestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="63"/>
+        <source>Images (*.png *.xpm *.svg)</source>
+        <translation>Afbeeldingen (*.png *.xpm *.svg)</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="151"/>
+        <source>Choose Desktop File</source>
+        <translation>Kies een .dekstop-bestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="179"/>
+        <location filename="../wingmenuconfiguration.cpp" line="215"/>
+        <source>No item selected</source>
+        <translation>Geen item gekozen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="179"/>
+        <source>Please select an item to edit.</source>
+        <translation>Kies een te bewerken item.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="215"/>
+        <source>Please select an item to remove.</source>
+        <translation>Kies een te verwijderen item.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="221"/>
+        <source>Edit Action</source>
+        <translation>Actie bewerken</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="237"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="238"/>
+        <source>Icon</source>
+        <translation>Pictogram</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="239"/>
+        <source>Command</source>
+        <translation>Opdracht</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="259"/>
+        <source>&quot;Command&quot; field is empty.</source>
+        <translation>Het opdrachtveld is blanco.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="255"/>
+        <source>&quot;Name&quot; field is empty.</source>
+        <translation>Het naamveld is blanco.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="39"/>
+        <source>Files are stored in: %1</source>
+        <translation>Bestanden worden opgeslagen in %1</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="153"/>
+        <source>All files (*)</source>
+        <translation>Alle bestanden (*)</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="162"/>
+        <source>Invalid desktop file</source>
+        <translation>Ongeldig .desktop-bestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="162"/>
+        <source>Selected file: %1 is invalid.</source>
+        <translation>‘%1’ is een ongeldig bestand.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="257"/>
+        <source>&quot;Icon&quot; field is empty.</source>
+        <translation>Het pictogramveld is blanco.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="260"/>
+        <source>Please fill all fields.</source>
+        <translation>Vul alle velden in.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="312"/>
+        <source>Choose Menu File</source>
+        <translation>Kies een menubestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="314"/>
+        <source>Menu files (*.menu)</source>
+        <translation>Menubestanden (*.menu)</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuconfiguration.cpp" line="342"/>
+        <source>Menu</source>
+        <translation>Menu</translation>
+    </message>
+</context>
+<context>
+    <name>WingMenuPlugin</name>
+    <message>
+        <location filename="../wingmenuplugin.cpp" line="71"/>
+        <source>Menu</source>
+        <translation>Menu</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuplugin.cpp" line="114"/>
+        <source>Show/hide main menu</source>
+        <translation>Hoofdmenu tonen/verbergen</translation>
+    </message>
+</context>
+<context>
+    <name>WingMenuWidget</name>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="68"/>
+        <source>Favorites list is empty, add items with the right-click menu from items on another category.</source>
+        <translation>De favorietenlijst is leeg. Voeg items toe door met de rechtermuisknop op een item te klikken.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="86"/>
+        <source>Search...</source>
+        <translation>Zoeken…</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="487"/>
+        <source>Move Up</source>
+        <translation>Omhoog verplaatsen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="497"/>
+        <source>Move Down</source>
+        <translation>Omlaag verplaatsen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="508"/>
+        <source>Remove from Favorites</source>
+        <translation>Verwijderen uit favorieten</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="512"/>
+        <source>Confirm removal</source>
+        <translation>Verwijderen bevestigen</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="512"/>
+        <source>Are you sure you want to remove
+%1:%2
+from Favorites?
+</source>
+        <translation>Weet u zeker dat u
+%1:%2
+wilt verwijderen uit de favorieten?
+</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="436"/>
+        <source>Add to Favorites</source>
+        <translation>Toevoegen aan favorieten</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="461"/>
+        <source>Add to desktop</source>
+        <translation>Snelkoppeling maken</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="467"/>
+        <source>Question</source>
+        <translation>Vraag</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="467"/>
+        <source>A file with the same name already exists.
+Do you want to overwrite it?</source>
+        <translation>Er is al een bestand met deze naam.
+Wilt u het bestand overschrijven?</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="471"/>
+        <source>Warning</source>
+        <translation>Waarschuwing</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="471"/>
+        <source>The file cannot be overwritten.</source>
+        <translation>Het bestand kan niet worden overschreven.</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="476"/>
+        <source>Copy</source>
+        <translation>Kopiëren</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="544"/>
+        <source>Favorites</source>
+        <translation>Favorieten</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="553"/>
+        <source>All Applications</source>
+        <translation>Alle programma&apos;s</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="776"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="778"/>
+        <source>Comment</source>
+        <translation>Opmerking</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="780"/>
+        <source>File</source>
+        <translation>Bestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="781"/>
+        <source>Category</source>
+        <translation>Categorie</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="782"/>
+        <source>Exec</source>
+        <translation>Uitvoerbaar bestand</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="959"/>
+        <source>Failed to start the application</source>
+        <translation>Het programma kan niet worden gestart</translation>
+    </message>
+    <message>
+        <location filename="../wingmenuwidget.cpp" line="960"/>
+        <source>%1 is not a valid .desktop file</source>
+        <translation>‘%1’ is geen geldig .desktop-bestand</translation>
+    </message>
+</context>
+</TS>
